6cfa5c7391d5cddda5f7137f6e61d2b02209c4d1
Ajay-ER <ajayzyan100@gmail.com>
2022-12-09 11:34:15 +0530

    new

--- assets/js/script.js
+++ assets/js/script.js
@@ -125,11 +125,7 @@ for (let i = 0; i < formInputs.length; i++) {
   formInputs[i].addEventListener("input", function () {
 
     // check form validation
-    if (form.checkValidity()) {
-      formBtn.removeAttribute("disabled");
-    } else {
-      formBtn.setAttribute("disabled", "");
-    }
+   
 
   });
 }
